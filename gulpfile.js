2df01f795ab229aed19597784dabc1b713a3c5c0
sunxiaoshen <sunxiaoshen@meituan.com>
2014-05-21 11:11:29 +0800

    multy watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,46 @@
 var gulp = require('gulp');
 var watchify = require('gulp-watchify');
-//var browserify = require('gulp-browserify');
+var browserify = require('gulp-browserify');
+var gutil = require('gulp-util');
+var filter = require('gulp-filter');
 var watch = require('gulp-watch');
 var path = require('path');
+
 var conf = require('./config.json');
 
 var workspace = conf.workspace;
+var cwd = process.cwd();
+workspace = path.relative(cwd, workspace);
 
 var destDir = path.join(workspace, conf.dest);
 var excludeReg = /^!.*/;
+
+// 调整文件地址为相对当前目录
 var srcArr = conf.src.map(function(item) {
     if (excludeReg.test(item)) {
-        return path.join('!', workspace, item.substr(1));
+        return '!' + path.join(workspace, item.substr(1));
     }
     return path.join(workspace, item);
 });
-var watching = true;
-gulp.task('default', watchify(function(watchify) {
+
+// 删除功能目前还不支持
+function isAddedOrChanged(file) {
+    return file.event === 'added' || file.event === 'changed';
+}
+// 生成当前已有文件的browserify目标文件
+gulp.task('watchify', watchify(function(watchify) {
     return gulp.src(srcArr)
         .pipe(watchify({
-            watch:watching
+            watch: false
         }))
         .pipe(gulp.dest(destDir))
+        .on('error', gutil.log);
 }))
+// 检测文件修改，生成browserify目标文件
+gulp.task('default', ['watchify'], function() {
+    watch({glob: srcArr})
+    .pipe(filter(isAddedOrChanged))
+    .pipe(browserify())
+    .pipe(gulp.dest(destDir))
+    .on('error', gutil.log);
+});
