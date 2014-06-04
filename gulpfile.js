0f82a565b8d85d3d62ff21054e61a88bc2153767
sunxiaoshen <sunxiaoshen@meituan.com>
2014-06-04 10:23:10 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
-var watchify = require('gulp-watchify');
 var browserify = require('gulp-browserify');
 var gutil = require('gulp-util');
+var clean = require('gulp-clean');
 var filter = require('gulp-filter');
 var watch = require('gulp-watch');
 var path = require('path');
+var fs = require('vinyl-fs');
 
 var conf = require('./config.json');
 
@@ -28,19 +29,27 @@ function isAddedOrChanged(file) {
     return file.event === 'added' || file.event === 'changed';
 }
 // 生成当前已有文件的browserify目标文件
-gulp.task('watchify', watchify(function(watchify) {
-    return gulp.src(srcArr)
-        .pipe(watchify({
-            watch: false
-        }))
+gulp.task('browserify', function() {
+
+    return fs.src(srcArr)
+        .pipe(browserify())
         .pipe(gulp.dest(destDir))
         .on('error', gutil.log);
-}))
+});
 // 检测文件修改，生成browserify目标文件
-gulp.task('default', ['watchify'], function() {
-    watch({glob: srcArr})
-    .pipe(filter(isAddedOrChanged))
-    .pipe(browserify())
-    .pipe(gulp.dest(destDir))
-    .on('error', gutil.log);
+gulp.task('default', ['browserify'], function() {
+    fs.watch(srcArr, function(files) {
+        var a = path.relative(cwd, files.path);
+        var b = path.relative(workspace, a);
+        var c = path.dirname(b);
+        if (files.type === 'changed' || files.type === 'added') {
+            gulp.src(path.relative(cwd, files.path))
+            .pipe(browserify())
+            .pipe(gulp.dest(path.join(destDir, c) ))
+            .on('error', gutil.log);
+        } else if (files.type === 'deleted') {
+            gulp.src(path.join(destDir, b)).pipe(clean({force: true}))
+            .on('error', gutil.log);
+        }
+    });
 });
